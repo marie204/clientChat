b1bef4d1c09a7b421a6966a7f93e09dd2a4e35c1
Unknown <niamor@zliton.com>
2017-11-14 11:13:07 +0100

    création d'un utilisateur ou utilisation d'un utilisateur déjà créé

--- js/script.js
+++ js/script.js
@@ -1,4 +1,27 @@
 
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
 function getUsers() {
 
 	parameters = {
@@ -28,4 +51,44 @@ function showUsers(users) {
 
 getUsers();
 
+var myUser;
 
+function createUser() {
+
+	parameters = {
+		url: 'http://messenger.api.niamor.com/createUser'
+	};
+
+	$.ajax(parameters).done(useUser);
+
+}
+
+function useUser(user) {
+	myUser = user;
+	setCookie('userAuthKey', user.authKey, 365);
+	$('#mask').hide();
+}
+
+var userAuthKey;
+
+function getUser() {
+	userAuthKey = getCookie('userAuthKey');
+	if ( userAuthKey != '' ) {
+		parameters = {
+			url: 'http://messenger.api.niamor.com/getUser',
+			method: 'post',
+			data: {
+				authKey: userAuthKey
+			}
+		};
+		$.ajax(parameters).done(function(user) {
+			myUser = user;
+			myUser.authKey = userAuthKey;
+			$('#mask').hide();
+		});
+	} else {
+		createUser();
+	}
+}
+
+getUser();
